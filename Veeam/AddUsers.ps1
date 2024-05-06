5878ecb7af5ddcf8d7907811855a0b1d70052b8c
nbonnet13880 <n.bonnet@outlook.com>
2024-05-06 20:26:50 +0200

    Add Veeam for M365 script

---
+++ Veeam/AddUsers.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+    Select M365 users with txt file, create backup job and add the users.
+.DESCRIPTION
+    The first command permit to keep information about the VBO Organization. The variable $Usernames read the txt file and store 
+    the username in the variable ($Usernames). The variable $ListUsers permit to store the attributes of the users.
+    THe item that you want backup (mailbox, archive mail, Onedrive et Personal Sharepoint) must be configured. If the calue configured
+    is $True, the item is backup ed. The last line of the script permit to create the backup jon on Veeam.
+.EXAMPLE
+    Before to run the script, the CmdLets for Veeam for M365 must be installed.
+.NOTES
+    Script created by Nicolas BONNET 06-05-2024.
+#>
+
+$M365Org=Get-VBOOrganization 
+$Usernames=Get-Content -Path C:\Folder\Your Users.txt
+$ListUsers=Get-VBOOrganizationUser -Organization $org | ?{$usernames.Contains($_.UserName)}
+$BackupJob=New-VBOBackupItem -User $ListUsers -Mailbox:$True -ArchiveMailbox:$true -OneDrive:$True -Sites:$True
+Add-VBOJob -Organization $M365Org -Name "[Daily] - Backup users" -Repository (Get-VBORepository) -SelectedItems $BackupJob
